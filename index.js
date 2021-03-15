304d04b13874af614dce1f1f7aff9980d5e7deba
Lukas Baumert <lukasbaumert1010@gmail.com>
2021-03-15 17:20:04 +0100

    changed dir structure, now /public is served

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const moment = require('moment');
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const moment = require('moment')
 
-const start = moment().format('DD.MM.YYYY, HH:mm');
-moment.locale('de');
+const start = moment().format('DD.MM.YYYY, HH:mm')
+moment.locale('de')
 
+app.use(express.static(__dirname + '/public'))
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+  res.sendFile(__dirname + '/public')
+})
 
 http.listen(3000, function () {
-    console.log('socket.io server listening on port 3000');
-    console.log('//================================\\\\');
-    console.log('|| Serverstart: ' + start + ' ||');
-    console.log('\\\\================================//');
-});
+  console.log('socket.io server listening on port 3000')
+  console.log('//================================\\\\')
+  console.log('|| Serverstart: ' + start + ' ||')
+  console.log('\\\\================================//')
+})
 
-io.on('connection', function(socket){
+let clients = [
+  {
+    user: '',
+    client: ''
+  }
+]
 
-    socket.on('request_hello', function(usr, id) {
+io.on('connection', function (socket) {
+  socket.on('request_hello', function (usr, id) {
+		// console.log(usr + ' with id: ' + id + ' send a request')
 
-        console.log(usr + ' with id: ' + id + ' send a request');
-        
-        io.sockets.connected[id].emit("greetings", "Howdy, " + usr + "!");
-    });
-});
+    clients.push({user: usr, client: id})
+    io.sockets.connected[id].emit('greetings', 'Howdy, ' + usr + '!')
+  })
+
+  socket.on('request_clients', function (id) {
+    io.sockets.connected[id].emit('deliver_clients', clients)
+  })
+})
